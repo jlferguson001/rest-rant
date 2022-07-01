f1541d337f8124416dbb20bec86b798fe7c7257a
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-07-01 11:02:29 -0500

    mongoose addition

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
+const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) },
